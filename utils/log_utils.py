83047012f51a2bf18be7a2b5fd81c7635eb6f7c3
isaaclee <hool19965401@gmail.com>
2024-03-27 03:18:22 +0800

    graylog testing(still testing)

--- utils/log_utils.py
+++ utils/log_utils.py
@@ -15,28 +15,31 @@ def set_logger(level=logging.INFO):
     # Get the current script name (without file extension)
     script_name = os.path.splitext(os.path.basename(sys.argv[0]))[0]
     # Create log file path
-    log_file_path = os.path.join("log", f"{script_name}.log")
+    log_directory = "/app/log"  # Adjust the log directory path according to your Docker volume setup
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+    log_file_path = os.path.join(log_directory, f"{script_name}.log")
 
-    # create log
+    # Create logger
     logger = logging.getLogger(__name__)
     logger.setLevel(level)
 
-    # log handle 
+    # File handler for logs
     file_handler = RotatingFileHandler(log_file_path, maxBytes=10*1024*1024, backupCount=5)
     file_handler.setLevel(level)
     file_format = logging.Formatter('%(asctime)s %(levelname)s - %(filename)s:%(lineno)d - %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z')
     file_handler.setFormatter(file_format)
     logger.addHandler(file_handler)
 
-    # log center handle 
+    # Console handler for logs
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
     console_format = logging.Formatter('%(asctime)s %(levelname)s - %(message)s', datefmt='%Y-%m-%dT%H:%M:%S%z')
     console_handler.setFormatter(console_format)
     logger.addHandler(console_handler)
 
-    # setup graylog
-    graylog_host = 'data_vacancy_insight.graylog.server.ip'
+    # Graylog handler setup
+    graylog_host = 'graylog'  # Use the service name defined in Docker Compose
     graylog_port = 12201  # GELF UDP port
 
     gelf_handler = graypy.GELFUDPHandler(graylog_host, graylog_port)
