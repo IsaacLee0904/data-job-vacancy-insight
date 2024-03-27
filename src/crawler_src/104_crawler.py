db8023366e47c725202a3875269f31c488e13497
isaaclee <hool19965401@gmail.com>
2024-03-28 01:19:38 +0800

    update search_keywords_list

--- src/crawler_src/104_crawler.py
+++ src/crawler_src/104_crawler.py
@@ -13,11 +13,11 @@ from utils.log_utils import set_logger
 logger = set_logger()
 
 # vacancy keywords 
-search_keywords = [ 'Busniess Analyst', 'BI' # BA
+search_keywords = [ 'Business Analyst', 'BI', 'BA' # BA
                   , 'Data Analyst', '資料分析師', '數據分析師' # DA
                   , 'Data Scientist', '資料科學家' # DS
                   , 'Data Engineer', '資料工程師', '數據工程師', '大數據工程師' # DE
-                  , 'Machine Learning Engineer', '機器學習工程師' # MLE
+                  , 'Machine Learning Engineer', 'Machine Learning', '機器學習工程師' # MLE
                   ]
  
 def main():
