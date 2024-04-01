ff86d343b3262bce1378ae37d019f2d01444bc89
IsaacLee0904 <hool19965401@gmail.com>
2024-04-01 17:22:48 +0800

    add database_connection_checker

---
+++ src/buildup_src/database_connection_checker.py
@@ -0,0 +1,13 @@
+import sys, os
+import toml
+
+# setup project root
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(project_root)
+
+from utils.log_utils import set_logger
+from utils.database_utils import DatabaseConnector
+
+logger = set_logger()
+connector = DatabaseConnector(logger)
+datawarehouse_conn = connector.connect_to_db('datawarehouse')
