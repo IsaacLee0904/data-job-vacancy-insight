252c26dacd1f014b30f72b84688770825e995d4e
IsaacLee0904 <hool19965401@gmail.com>
2024-04-01 18:37:01 +0800

    add create raw data table function

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -37,3 +37,58 @@ class DatabaseConnector:
         except Exception as e:
             self.logger.error(f"Error connecting to {db_name}: {e}")
             return None
+
+class DatabaseOperation:
+    def __init__(self, connection, logger):
+        """
+        Initialize the DatabaseOperation with a database connection and a logger.
+
+        Parameters:
+        - connection: The connection object to the database.
+        - logger (logging.Logger): A Logger object for logging information and errors.
+        """
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+        self.logger = logger
+
+    def create_table(self, create_table_query):
+        """
+        Create a table in the database based on the provided SQL query.
+
+        Parameters:
+        - create_table_query (str): The SQL query to create a table.
+        """
+        try:
+            self.cursor.execute(create_table_query)
+            self.connection.commit()
+            self.logger.info("Table created successfully.")
+        except psycopg2.Error as e:
+            self.logger.error(f"An error occurred while creating the table: {e}")
+            self.connection.rollback()
+
+def create_rawdata_table(logger):
+    connector = DatabaseConnector(logger)
+    connection = connector.connect_to_db('datawarehouse')
+    db_operation = DatabaseOperation(connection, logger)
+
+    job_listings_table_query = """
+    CREATE TABLE IF NOT EXISTS job_listings (
+        id SERIAL PRIMARY KEY,
+        job_title VARCHAR(255) NOT NULL,
+        company_name VARCHAR(255) NOT NULL,
+        salary VARCHAR(255),
+        location VARCHAR(255),
+        job_description TEXT,
+        job_type VARCHAR(50),
+        degree_required VARCHAR(255),
+        major_required VARCHAR(255),
+        experience VARCHAR(255),
+        skill TEXT,
+        tools TEXT,
+        others TEXT,
+        url TEXT,
+        crawl_date DATE
+    );
+    """
+    db_operation.create_table(job_listings_table_query)
+    connection.close()
