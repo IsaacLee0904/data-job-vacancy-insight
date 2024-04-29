cba488a1b4c8d2108cb5a27b5ca7dfae668be891
isaaclee <hool19965401@gmail.com>
2024-04-29 09:01:19 +0800

    update airflow dags

--- airflow_dags/data_pipeline_dag.py
+++ airflow_dags/data_pipeline_dag.py
@@ -6,10 +6,10 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2024, 4, 29),  # Set start date to April 29, 2024
+    'start_date': datetime(2023, 1, 1),  # Set start date to April 22, 2024
     'email': ['hool19965401@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'email_on_failure': True,
+    'email_on_retry': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -22,15 +22,29 @@ dag = DAG(
     catchup=False,
 )
 
+# task : crawl data from 104 
+crawl_data = DockerOperator(
+    task_id='crawl_data',
+    image='data-job-vacancy-insight-app',
+    api_version='auto',
+    auto_remove=True,
+    command='python src/crawler_src/104_crawler.py',
+    docker_url='unix://var/run/docker.sock',  # Docker Daemon socket
+    network_mode='bridge',
+    working_dir='/app',  
+    dag=dag,
+)
+
 # task : load raw data in Docker 
 load_raw_data = DockerOperator(
     task_id='load_raw_data',
     image='data-job-vacancy-insight-app',
     api_version='auto',
-    auto_remove=False,
+    auto_remove=True,
     command='python src/data_processing_src/load_raw_data.py',
     docker_url='unix://var/run/docker.sock',  # Docker Daemon socket
     network_mode='bridge',
+    working_dir='/app',  
     dag=dag,
 )
 
@@ -39,10 +53,11 @@ transform_raw_data = DockerOperator(
     task_id='transform_raw_data',
     image='data-job-vacancy-insight-app',
     api_version='auto',
-    auto_remove=False,
+    auto_remove=True,
     command='python src/data_processing_src/transform_raw_data.py',
     docker_url='unix://var/run/docker.sock',  # Docker Daemon socket
     network_mode='bridge',
+    working_dir='/app',   
     dag=dag,
 )
 
@@ -51,11 +66,12 @@ run_dbt = DockerOperator(
     task_id='run_dbt',
     image='data-job-vacancy-insight-app',
     api_version='auto',
-    auto_remove=False,
+    auto_remove=True,
     command='bash -c "cd dbt && dbt run && dbt test && dbt snapshot"',
     docker_url='unix://var/run/docker.sock',  # Docker Daemon socket
     network_mode='bridge',
+    working_dir='/app',  
     dag=dag,
 )
 
-load_raw_data >> transform_raw_data >> run_dbt
+crawl_data >> load_raw_data >> transform_raw_data >> run_dbt
