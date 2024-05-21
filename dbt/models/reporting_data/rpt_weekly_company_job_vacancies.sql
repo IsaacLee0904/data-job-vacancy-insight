fbd8d19571232ec538c85719ef36c7f5d4d70998
isaaclee <hool19965401@gmail.com>
2024-05-22 00:30:25 +0800

    dbt - rpt_weekly_company_job_vacancies (Done)

---
+++ dbt/models/reporting_data/rpt_weekly_company_job_vacancies.sql
@@ -0,0 +1,21 @@
+-- models/reporting_data/rpt_weekly_company_job_vacancies.sql
+{{ config(materialized='incremental', schema='reporting_data') }}
+
+SELECT 
+    RANK() OVER (ORDER BY AAA.opening_count DESC) AS rank
+    , BBB.company_name
+    , AAA.opening_count
+    , AAA.crawl_date    
+FROM (
+    SELECT 
+        AA.company_id
+        , COUNT(AA.company_id) AS opening_count
+        , AA.crawl_date
+    FROM modeling_data.er_job AA
+    WHERE AA.crawl_date = '{{modules.datetime.date.today().strftime('%Y-%m-%d')}}'
+    GROUP BY AA.company_id, AA.crawl_date
+) AAA
+LEFT JOIN modeling_data.er_company BBB ON AAA.company_id = BBB.company_id 
+ORDER BY AAA.opening_count DESC
+
+
