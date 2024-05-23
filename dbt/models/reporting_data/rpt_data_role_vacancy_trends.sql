21dd72af93d2716aed25f5c9886b838632bb5aaf
IsaacLee0904 <hool19965401@gmail.com>
2024-05-23 18:30:11 +0800

    rpt_data_role_vacancy_trends (Done)

--- dbt/models/reporting_data/rpt_data_role_vacancy_trends.sql
+++ dbt/models/reporting_data/rpt_data_role_vacancy_trends.sql
@@ -5,6 +5,7 @@ SELECT
     data_role
     , count(data_role) AS count
     , crawl_date 
+FROM {{ source('modeling_data', 'er_job') }}
 WHERE crawl_date = '{{modules.datetime.date.today().strftime('%Y-%m-%d')}}'
 GROUP BY data_role, crawl_date 
 ORDER BY count(data_role) DESC
