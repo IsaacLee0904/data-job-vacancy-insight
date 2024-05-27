e476fa22a382f985fa7174149674dc59b8227b6b
IsaacLee0904 <hool19965401@gmail.com>
2024-05-27 14:02:49 +0800

    rpt_job_openings_geograph (Done)

--- dbt/models/reporting_data/rpt_job_openings_geograph.sql
+++ dbt/models/reporting_data/rpt_job_openings_geograph.sql
@@ -1,14 +1,16 @@
+-- models/reporting_data/rpt_job_openings_geograph.sql
+{{ config(materialized='incremental', schema='reporting_data', unique_key='county_name_eng || district_name_eng || crawl_date') }}
+
 WITH job_with_company_location AS (
     SELECT 
-        AAA.unique_id,
         AAA.company_id,
         BBB.company_name, 
         CCC.county_id,
         CCC.district_id
-    FROM modeling_data.er_job AAA
-    LEFT JOIN modeling_data.er_company BBB ON AAA.company_id = BBB.company_id
-    LEFT JOIN modeling_data.er_company_location CCC ON AAA.company_id = CCC.company_id
-    WHERE AAA.crawl_date = '2024-04-01'    
+    FROM {{ source('modeling_data', 'er_job') }} AAA
+    LEFT JOIN {{ source('modeling_data', 'er_company') }} BBB ON AAA.company_id = BBB.company_id
+    LEFT JOIN {{ source('modeling_data', 'er_company_location') }} CCC ON AAA.company_id = CCC.company_id
+    WHERE AAA.crawl_date = '{{modules.datetime.date.today().strftime('%Y-%m-%d')}}'   
 ),
 county_job_counts AS (
     SELECT 
@@ -17,7 +19,7 @@ county_job_counts AS (
         J.district_id,
         COUNT(*) AS job_count
     FROM job_with_company_location J
-    GROUP BY J.company_id, J.county_id, J.district_id
+    GROUP BY J.company_id, J.county_id, J.district_id 
 ),
 ranked_counties AS (
     SELECT 
@@ -26,14 +28,15 @@ ranked_counties AS (
     FROM county_job_counts C
 )
 SELECT 
-    E.county_name_eng,
-    D.district_name_eng,
-    R.job_count
+    E.county_name_eng
+    , D.district_name_eng
+    , SUM(R.job_count) AS openings_count
+    , '{{modules.datetime.date.today().strftime('%Y-%m-%d')}}' AS crawl_date 
 FROM ranked_counties R
-LEFT JOIN modeling_data.er_county E ON R.county_id = E.county_id
-LEFT JOIN modeling_data.er_district D ON R.district_id = D.district_id
-WHERE R.rank = 1;
+LEFT JOIN {{ source('modeling_data', 'er_county') }} E ON R.county_id = E.county_id
+LEFT JOIN {{ source('modeling_data', 'er_district') }} D ON R.district_id = D.district_id
+WHERE R.rank = 1
+GROUP BY E.county_name_eng, D.district_name_eng
+ORDER BY SUM(R.job_count) DESC
 
-select count(*)
-from modeling_data.er_job ej 
-where crawl_date = '2024-04-01'
+
