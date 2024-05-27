7f32e9227873ccb72660aa48e5d1833e5d5d79eb
IsaacLee0904 <hool19965401@gmail.com>
2024-05-27 18:34:17 +0800

    add dashboard template

--- src/dashboard_src/dashboard_app.py
+++ src/dashboard_src/dashboard_app.py
@@ -1,4 +1,5 @@
 import dash
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import html, dcc
 from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+
