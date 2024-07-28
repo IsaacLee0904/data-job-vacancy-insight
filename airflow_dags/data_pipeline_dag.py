9bdd64d740b3aaee02ac83312a07a6e405e9eb9e
isaaclee <hool19965401@gmail.com>
2024-07-29 01:32:52 +0800

    update airflow dags fix tg time error

--- airflow_dags/data_pipeline_dag.py
+++ airflow_dags/data_pipeline_dag.py
@@ -5,22 +5,22 @@ from datetime import datetime, timedelta
 import pendulum
 
 def notify_failure(context):
-    execution_date  = context['execution_date'].strftime('%Y-%m-%d %H:%M:%S')
+    execution_date  = pendulum_datetime.from_timestamp(context['execution_date'].timestamp(), tz='Asia/Taipei').strftime('%Y-%m-%d')
     failed_alert = TelegramOperator(
         task_id='send_failure_alert',
         telegram_conn_id='telegram_bot',
-        chat_id='*********', # need to replace with your chat_id
-        text=f"🚨 Alert! Airflow weekly scheduled `job_vacancy_data_pipeline` has failed.\n\n🛠 Task: {context['task_instance_key_str']}\n\n📅 Execution Date: {execution_date}\n\n❌ Status: Failed\n\nPlease check the logs and resolve the issues.",
+        chat_id='*********',
+        text=f"🚨 Alert! Airflow weekly scheduled `job_vacancy_data_pipeline` has failed.\n\n🛠 Task: {context['task_instance_key_str']}\n\n📅 Execution Date: {execution_date}\n❌ Status: Failed\n\nPlease check the logs and resolve the issues.",
         dag=dag
     )
     return failed_alert.execute(context=context)
 
 def notify_success(context):
-    execution_date  = context['execution_date'].strftime('%Y-%m-%d %H:%M:%S')
+    execution_date  = pendulum_datetime.from_timestamp(context['execution_date'].timestamp(), tz='Asia/Taipei').strftime('%Y-%m-%d')
     success_alert = TelegramOperator(
         task_id='send_success_alert',
         telegram_conn_id='telegram_bot',
-        chat_id='*********', # need to replace with your chat_id
+        chat_id='*********',
         text=f"🎉 Congratulations! Airflow weekly scheduled `job_vacancy_data_pipeline` has successfully completed!\n\n🔍 Please review the results and ensure all data processing went smoothly.\n\n📅 Execution Date: {execution_date}\n\n✅ Status: Success",
         dag=dag
     )
@@ -68,7 +68,7 @@ transform_raw_data = BashOperator(
 
 # task：run dbt in Docker
 run_dbt = BashOperator(
-    task_id='build_er_model_with_dbt',
+    task_id='transform_data_with_dbt',
     bash_command='docker exec -i data-job-vacancy-insight-app-1 bash -c "cd dbt && dbt run && dbt test && dbt snapshot"',
     dag=dag,
 )
