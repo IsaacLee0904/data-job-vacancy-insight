8799016ca0f3f0728c75797079fcaae8748c11cf
isaaclee <hool19965401@gmail.com>
2024-07-29 22:07:25 +0800

    dbt : train_user_recommendations (Done)

--- dbt/models/training_data/train_user_recommendations.sql
+++ dbt/models/training_data/train_user_recommendations.sql
@@ -1,1 +1,17 @@
 -- models/training_data/train_user_recommendations.sql
+{{ config(materialized='table', schema='training_data', unique_key='unique_id || crawl_date')}}
+
+SELECT 
+	AAA.unique_id 
+	, AAA.data_role 
+	, AAA.job_title 
+	, BBB.company_name
+	, AAA.salary 
+	, AAA.job_description 
+	, AAA.experience
+	, AAA.tools 
+	, AAA."others" 
+	, AAA.url 
+    , AAA.crawl_date
+FROM {{ source('modeling_data', 'er_job') }}AAA
+LEFT JOIN (SELECT * FROM {{ source('modeling_data', 'er_company') }}BB)BBB on AAA.company_id = BBB.company_id
