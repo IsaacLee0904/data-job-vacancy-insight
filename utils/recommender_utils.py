5ad2ce3854f699628d2a9172d6d734905795af23
isaaclee <hool19965401@gmail.com>
2024-08-04 23:55:01 +0800

    add read pdf funciton

---
+++ utils/recommender_utils.py
@@ -0,0 +1,9 @@
+import PyPDF2
+
+def extract_text_from_pdf(pdf_path):
+    with open(pdf_path, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ''
+        for page in range(len(reader.pages)):
+            text += reader.pages[page].extract_text()
+    return text
